45baf45142815859cd8fd1bf1fda8a9264c0e320
Kirill Kondratenko <cybem@cybem.info>
2015-07-14 21:01:14 +0300

    Version 0.4.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cyanite-remover "0.4.0"
+(defproject cyanite-remover "0.4.1-SNAPSHOT"
   :description "Cyanite data removal tool"
   :url "https://github.com/cybem/cyanite-remover"
   :license {:name "MIT License"
